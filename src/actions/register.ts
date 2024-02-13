a45d8f785772ac552fc70cd2745b970ffbb99248
Tinn-K <tinn.kanjananuwat@gmail.com>
2024-02-14 00:33:54 +0700

    fix: update bcrypt import to bcryptjs

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -3,7 +3,7 @@
 import { getUserByEmail } from "@/data/user";
 import { RegisterSchema } from "@/schemas";
 import { db } from "@/server/db";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import * as z from "zod";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
