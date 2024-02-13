95cebbaefe5929f3bb19c1c44e2d2083b7199245
Tinn-K <tinn.kanjananuwat@gmail.com>
2024-02-13 22:04:12 +0700

    feat: add register action to handle user registration

---
+++ src/actions/register.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { getUserByEmail } from "@/data/user";
+import { RegisterSchema } from "@/schemas";
+import { db } from "@/server/db";
+import bcrypt from "bcrypt";
+import * as z from "zod";
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  const { name, email, password, confPassword } = validatedFields.data;
+
+  if (!(password === confPassword)) return { error: "Password does not match" };
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
+};
