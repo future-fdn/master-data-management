3ef4062c50ab55504784a7a66f09dcb1c85278db
Tinn-K <tinn.kanjananuwat@gmail.com>
2024-02-13 21:53:47 +0700

    feat: add login functionality and server routes

---
+++ src/server/routes.ts
@@ -0,0 +1,6 @@
+export const publicRoutes = ["/auth/login", "/auth/register"];
+
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+export const apiPrefix = "/api";
+export const DEFAULT_LOGIN_REDIRECT = "/";
