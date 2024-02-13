9303945fb8c753dad935e1f72d95fbfa8285c246
Tinn-K <tinn.kanjananuwat@gmail.com>
2024-02-14 00:07:57 +0700

    feat: add user validation and session customization

--- src/server/auth.config.ts
+++ src/server/auth.config.ts
@@ -1,11 +1,26 @@
+import bcrypt from "bcryptjs";
 import { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { getUserByEmail } from "../data/user";
+import { LoginSchema } from "../schemas";
 
 export default {
   providers: [
     Credentials({
       authorize: async (credentials) => {
-        console.log(credentials);
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
+        }
+
         return null;
       },
     }),
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -2,11 +2,54 @@ import { db } from "@/server/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth, { NextAuthConfig, type DefaultSession } from "next-auth";
 
+import { getAccountByUserId } from "@/data/account";
+import { getUserById } from "@/data/user";
 import authConfig from "@/server/auth.config";
+import { UserRole } from "@prisma/client";
 
 const authOptions: NextAuthConfig = {
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
+  callbacks: {
+    async signIn({ user, account }) {
+      const existingUser = await getUserById(user?.id || "");
+
+      return !!existingUser;
+    },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email ?? "";
+        session.user.isOAuth = token.isOAuth as boolean;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   ...authConfig,
 };
 
