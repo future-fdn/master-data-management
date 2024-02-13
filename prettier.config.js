90223c829143b7917a4d89777d4c5e8c0f8cf2b5
Tinn-K <tinn.kanjananuwat@gmail.com>
2024-02-13 22:29:10 +0700

    chore: update prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -3,6 +3,12 @@ const config = {
   plugins: ["prettier-plugin-tailwindcss"],
   semi: true,
   singleQuote: false,
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxSingleQuote: false,
 };
 
 export default config;
