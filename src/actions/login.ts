3979c2d54297c0a741e4752502e35c56e083ea1b
Tinn-K <tinn.kanjananuwat@gmail.com>
2024-02-16 03:36:31 +0700

    style: fix eslint suggestion

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -5,7 +5,7 @@ import { LoginSchema } from "@/schemas";
 import { signIn } from "@/server/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/server/routes";
 import { AuthError } from "next-auth";
-import * as z from "zod";
+import type * as z from "zod";
 
 export const login = async (
   values: z.infer<typeof LoginSchema>,
@@ -21,7 +21,7 @@ export const login = async (
 
   const existingUser = await getUserByEmail(email);
 
-  if (!existingUser || !existingUser.email || !existingUser.password) {
+  if (!existingUser?.email || !existingUser.password) {
     return { error: "Email does not exist!" };
   }
 
@@ -29,11 +29,10 @@ export const login = async (
     await signIn("credentials", {
       email,
       password,
-      redirectTo: callbackUrl || DEFAULT_LOGIN_REDIRECT,
+      redirectTo: callbackUrl ?? DEFAULT_LOGIN_REDIRECT,
     });
   } catch (error) {
     if (error instanceof AuthError) {
-      console.log(error);
       switch (error.type) {
         case "CredentialsSignin":
           return { error: "Invalid credentials!" };
